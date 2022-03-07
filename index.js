636578e9268c6afda32e3aec77595dadd03eee26
ItsJustTomDev <96871598+ItsJustTomDev@users.noreply.github.com>
2022-03-07 17:37:16 +0200

    Added API functionallity

--- index.js
+++ index.js
@@ -1,46 +1,162 @@
-// Express init
-const express = require("express");
-const app = express();
+// Initialize Libraries
+const express = require("express"),
+  bodyParser = require("body-parser"),
+  uuid = require("uuid"),
+  app = express();
 
-// Import Morgan middleware lib
-const morgan = require("morgan"); 
+app.use(bodyParser.json());
 
-// Log basic request data in the terminal with Morgan
-app.use(morgan("common"));
-
-// Array that holds object of movies
 let movies = [
   {
-    name: "Movie 1",
-    director: "Director 1",
+    Title: "Hitman 47",
+    Director: "Aleksander Bach",
+    Genre: {
+      Name: "Action",
+      Description:
+        "Agent 47 (Rupert Friend), known only by the bar code tattoo on his neck, is an elite international assassin. Engineered to be an unstoppable killing machine, 47 is contracted by a covert organization to assassinate high-profile targets on a global scale.",
+    },
   },
   {
-    name: "Movie 2",
-    director: "Director 2",
-  },
-  {
-    name: "Movie 3",
-    director: "Director 3",
+    Title: "Shrek",
+    Director: "Andrew Adamson",
+    Genre: {
+      Name: "Adventure",
+      Description:
+        "Though surly, dangerous, cynical, misanthropic, and venomously cranky, Shrek is peaceful and does not care to hurt anyone, but just wants to live in solitude and be left alone. Shrek is accompanied by Donkey, an excitable and hyperactive talking donkey.",
+    },
   },
 ];
 
-// GET movies JSON object for "/movies" request URL
+let users = [
+  {
+    id: 1,
+    name: "Kim",
+    favoriteMovies: [],
+  },
+  {
+    id: 2,
+    name: "Joe",
+    favoriteMovies: ["Hitman 47"],
+  },
+];
+
+// CREATE
+app.post("/users", (req, res) => {
+  const newUser = req.body;
+
+  if (newUser.name) {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).json(newUser);
+  } else {
+    res.status(400).send("users need names");
+  }
+});
+
+// UPDATE
+app.put("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const updatedUser = req.body;
+
+  let user = users.find((user) => user.id == id);
+
+  if (user) {
+    user.name = updatedUser.name;
+    res.status(200).json(user);
+  } else {
+    res.status(400).send("No user has been found");
+  }
+});
+
+// CREATE
+app.post("/users/:id/:movieTitle", (req, res) => {
+  const { id, movieTitle } = req.params;
+
+  let user = users.find((user) => user.id == id);
+
+  if (user) {
+    user.favoriteMovies.push(movieTitle);
+    res
+      .status(200)
+      .send(`${movieTitle} has been added to the user ${id}'s array`);
+  } else {
+    res.status(400).send("No user has been found");
+  }
+});
+
+// DELETE
+app.delete("/users/:id/:movieTitle", (req, res) => {
+  const { id, movieTitle } = req.params;
+
+  let user = users.find((user) => user.id == id);
+
+  if (user) {
+    user.favoriteMovies = user.favoriteMovies.filter(
+      (title) => title !== movieTitle
+    );
+    res
+      .status(200)
+      .send(`${movieTitle} has been removed from from user ${id}'s array`);
+  } else {
+    res.status(400).send("No user has been found");
+  }
+});
+
+// DELETE
+app.delete("/users/:id", (req, res) => {
+  const { id } = req.params;
+
+  let user = users.find((user) => user.id == id);
+
+  if (user) {
+    users = users.filter((user) => user.id != id);
+    res.status(200).send(`user ${id} has been removed`);
+  } else {
+    res.status(400).send("No user has been found");
+  }
+});
+
+// READ
 app.get("/movies", (req, res) => {
-  res.json(movies);
+  res.status(200).json(movies);
 });
 
-// GET message for "/" request URL
-app.get("/", (req, res) => {
-  res.send("Hello from MyFlix!");
+// READ
+app.get("/movies/:title", (req, res) => {
+  const { title } = req.params;
+  const movie = movies.find((movie) => movie.Title === title);
+
+  if (movie) {
+    res.status(200).json(movie);
+  } else {
+    res.status(400).send("No movie has been found");
+  }
 });
 
-// Static content from 'public' directory
-app.use(express.static("public"));
+// READ
+app.get("/movies/genre/:genreName", (req, res) => {
+  const { genreName } = req.params;
+  const genre = movies.find((movie) => movie.Genre.Name === genreName)?.Genre;
 
-// Error handling
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Some error has occured");
+  if (genre) {
+    res.status(200).json(genre);
+  } else {
+    res.status(400).send("No genre has been found");
+  }
 });
 
-app.listen(3000, () => console.log("App is listening on port 3000"));
+// READ
+app.get("/movies/directors/:directorName", (req, res) => {
+  const { directorName } = req.params;
+  const director = movies.find(
+    (movie) => movie.Director === directorName
+  )?.Director;
+
+  if (director) {
+    res.status(200).json(director);
+  } else {
+    res.status(400).send("No director has been found");
+  }
+});
+
+app.listen(8080, () => console.log("Listening on port 8080"));
