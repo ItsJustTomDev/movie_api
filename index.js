309c55f0652229c04294a36e3f8768fa742b069a
ItsJustTomDev <96871598+ItsJustTomDev@users.noreply.github.com>
2022-04-01 18:24:01 +0300

    cors

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ let allowedOrigins = [
   "http://localhost:8080",
   "http://testsite.com",
   "http://localhost:1234",
-  "https://624715e9eb349f00081d36c7--harmonious-gecko-34803e.netlify.app",
+  "https://624718dc5275270008f24935--harmonious-gecko-34803e.netlify.app/",
 ];
 app.use(
   cors({
